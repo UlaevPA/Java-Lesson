3e88fdb9472b66641fc8a86b1bfd073e17c8b454
ulaev <ulaev1990@mail.ru>
2023-11-30 21:10:08 +0300

    Add task4

---
+++ Lesson_1/task_4.java
@@ -0,0 +1,17 @@
+// Напишите метод, который находит самую длинную строку общего
+// префикса среди массива строк.
+// Если общего префикса нет, вернуть пустую строку ""
+
+
+package Lesson_1;
+
+public class task_4 {
+    String phrase = "Добро пожаловать на курс по Java";
+    String[] arr = phrase.split(" ");
+    StringBuilder result = new StringBuilder();
+    for (int i = arr.length - 1; i >= 0 ; i--) {
+        result.append(arr[i]).append(" ");
+    }
+    System.out.println(result.toString().trim());
+    
+}
