b8106bb15b745d82840a68fd7c858559322d346e
ulaev <ulaev1990@mail.ru>
2023-11-30 20:33:08 +0300

    Add task1

--- Lesson_1/task_1.java
+++ Lesson_1/task_1.java
@@ -1,11 +1,31 @@
 package Lesson_1;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Scanner;
+
 /**
  * task_1
  */
 public class task_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.err.println("Введите ваше имя: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        LocalTime time = LocalTime.now();
+        int hour = time.getHour();
+        if(hour >= 5 && hour < 12){
+        System.out.println("Доброе утро, " + name + "!");
+        } else if(hour >= 5 && hour < 12){
+        System.out.println("Добрый день, " + name + "!");;
+        
+        } else if(hour >= 5 && hour < 12){
+        System.out.println("Добрый вечер, " + name + "!");
+        } else{
+        System.out.println("Доброй ночи, " + name + "!");
+        }
+
     }
     
+       
 }
